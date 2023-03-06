2ddc326ce29bc40aeb71e32fa8963ca3fe58220a
lksAnton <lukusaanton@gmail.com>
2023-03-06 22:16:40 +0100

    commit

---
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -0,0 +1,20 @@
+package com.example.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/")
+    @ResponseBody
+    public String doctor(
+            @RequestParam(required = false, defaultValue = "0") int number,
+            @RequestParam(required = false, defaultValue = "John Smith") String name) {
+        // doctor = new Doctor(number, name);
+
+        return "Id : " + number + " Name : " + name;
+    }
+}
